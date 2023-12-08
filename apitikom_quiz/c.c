78f89585ad70b767b2ea07f64e8759fb1a7d0e9b
krisnabimantoro <krisnabmntr@gmail.com>
2023-12-08 08:37:45 +0700

    error_quiz

---
+++ apitikom_quiz/c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Buat perulangan for ini untuk mencetak angka 1 hingga 5
+    for (int i = 1; i++; i <= 5) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
---
+++ apitikom_quiz/d.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[4] = {10, 20, 30, 40, 50};
+
+    printf("Output nilai yang ke-5 adalah: %d\n" numbers[0]);
+
+    return 0;
+}
